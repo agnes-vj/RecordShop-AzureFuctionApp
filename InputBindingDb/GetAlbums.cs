11a0e2f8f500588287024d7bd42deaf056f616e1
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-03-18 12:13:42 +0000

    Add function to access Album by Id

--- InputBindingDb/GetAlbums.cs
+++ InputBindingDb/GetAlbums.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using InputBindingDb.Model;
 using Microsoft.AspNetCore.Mvc;
 using InputBindingDb.Services;
+using Microsoft.AspNetCore.Http;
 namespace InputBindingDb
 {
     public class GetAlbumsFunction
@@ -31,8 +32,35 @@ namespace InputBindingDb
             var response = _albumsService.GetAllAlbums();
             List<AlbumDTO> albums = response.albumDTOs;
             logger.LogInformation($"{albums.Count} albums fetched!");
-            return new OkObjectResult(albums);
+            return response.status switch
+            {
+                ExecutionStatus.SUCCESS => new OkObjectResult(albums),
+                ExecutionStatus.NOT_FOUND => new NotFoundObjectResult("No Albums Found"),
+                _ => new ObjectResult("Internal Server error")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                }
+            };
         }
-
+        [Function("GetAlbumById")]
+        public IActionResult GetAlbumById(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "albums/{id}")] HttpRequestData req,
+        FunctionContext context, int id)
+        {
+            var logger = context.GetLogger("GetAlbumsFunction");
+            logger.LogInformation($"Fetching albums from SQL Server Express with Id {id}.....");
+            var response = _albumsService.GetAlbumById(id);
+            AlbumDTO album = response.albumDTO;
+            logger.LogInformation($"{album?.Title ?? "0 album"} fetched!");
+            return response.status switch
+            {
+                ExecutionStatus.SUCCESS => new OkObjectResult(album),
+                ExecutionStatus.NOT_FOUND => new NotFoundObjectResult("No Albums Found"),
+                _ => new ObjectResult("Internal Server error")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                }
+            };
+        }
     }
 }
