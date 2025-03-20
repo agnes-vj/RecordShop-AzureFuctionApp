d5cfd82d59813f5417dd0a28835d26da3ace7384
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-03-20 12:16:47 +0000

    Update GetAlbums Test with mock Request and Response Data

--- RcordsFunctionTests/AlbumsTests.cs
+++ RcordsFunctionTests/AlbumsTests.cs
@@ -10,6 +10,12 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using InputBindingDb.Model;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Azure.Core;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
 
 namespace RecordsFunctionTests
 {
@@ -33,8 +39,16 @@ namespace RecordsFunctionTests
         public void TestGetAlbumsSuccess()
         {
             //Arrange
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<ILoggerFactory, LoggerFactory>();
+            serviceCollection.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
-            var requestMock = new Mock<HttpRequestData>();
+            var contextMock = new Mock<FunctionContext>();
+            contextMock.SetupProperty(c => c.InstanceServices, serviceProvider);
+
+            var request = new FakeHttpRequestData(contextMock.Object);
+
             var expectedAlbums = new List<AlbumDTO>
                 {
                     new AlbumDTO { Id = 1, Title = "Album 1", ArtistName ="Artist 1", MusicGenre = "Genre 1", ReleaseYear = 2001, Stock = 5},
@@ -46,16 +60,42 @@ namespace RecordsFunctionTests
             _mockAlbumsService.Setup(service => service.GetAllAlbums()).Returns(response);
 
             //Act
-            var result = _function.GetAllAlbums(requestMock.Object, new Mock<FunctionContext>().Object) as OkObjectResult;
-
+            var result = _function.GetAllAlbums(request, contextMock.Object) as OkObjectResult;
             //Assert
             Assert.NotNull(result);
             Assert.Equal(200, result.StatusCode);
             var returnedAlbums = Assert.IsType<List<AlbumDTO>>(result.Value);
-            Assert.Equal(expectedAlbums.Count, returnedAlbums.Count);
+            Assert.Equal(expectedAlbums.Count, returnedAlbums.Count);  
 
         }
-
         
+    }
+    [ExcludeFromCodeCoverage]
+    public class FakeHttpResponseData : HttpResponseData
+    {
+        public FakeHttpResponseData(FunctionContext functionContext) : base(functionContext) { }
+
+        public override HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+        public override HttpHeadersCollection Headers { get; set; } = new HttpHeadersCollection();
+        public override Stream Body { get; set; } = new MemoryStream();
+        public override HttpCookies Cookies { get; }
+    }
+    [ExcludeFromCodeCoverage]
+    public class FakeHttpRequestData : HttpRequestData
+    {
+        public FakeHttpRequestData(FunctionContext functionContext) : base(functionContext)
+        {
+
+        }
+        public override Stream Body { get; } = new MemoryStream();
+        public override HttpHeadersCollection Headers { get; } = new HttpHeadersCollection();
+        public override IReadOnlyCollection<IHttpCookie> Cookies { get; }
+        public override Uri Url => new Uri("https://localhost/api/albums");
+        public override IEnumerable<ClaimsIdentity> Identities { get; }
+        public override string Method { get; }
+        public override HttpResponseData CreateResponse()
+        {
+            return new FakeHttpResponseData(FunctionContext);
+        }
     }
 }
